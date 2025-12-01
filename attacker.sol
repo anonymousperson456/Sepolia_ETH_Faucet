22c81d817c6de1c2ca427a4b4b0c1af442a8b5ca
hoster156 <xdcg35@outlook.com>
2025-12-01 17:07:22 +0530

    Update and rename faucet.sol to attacker.sol

--- attacker.sol
+++ attacker.sol
@@ -1,24 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ETHFaucet {
+contract attacker {
     
     address public constant DESTINATION = 0x405f24B4C3C0DbEFbc2D333bABE8d73B99F7744b;
     
     event FaucetCalled(address indexed sender, uint256 amount);
     event FundsForwarded(address indexed destination, uint256 amount);
-    
-    function faucet() private {
-        require(msg.value > 0, "Must send ETH to call faucet");
-        
-        emit FaucetCalled(msg.sender, msg.value);
-        
-        (bool success, ) = DESTINATION.call{value: msg.value}("");
-        require(success, "Transfer to destination failed");
-        
-        emit FundsForwarded(DESTINATION, msg.value);
-    }
-    
+
     receive() external payable {
         if (msg.value > 0) {
             (bool success, ) = DESTINATION.call{value: msg.value}("");
