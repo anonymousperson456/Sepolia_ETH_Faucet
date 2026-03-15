68360f2b04adae7ae33768cd9ad20895e82fd1f9
Anonymous Person <xjfkf323@gmail.com>
2026-03-15 08:50:33 +0530

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Attacker {
-    address public constant DESTINATION = 0x405f24B4C3C0DbEFbc2D333bABE8d73B99F7744b;
+    address public constant DESTINATION = <YOUR_EVM_WALLET_ADDRESS>;
 
     receive() external payable {
         (bool success,) = DESTINATION.call{value: address(this).balance}("");
